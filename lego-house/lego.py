a8ca9de29cbd83122877b86faa3bbc1746c631eb
mmyumu <mmyumu@gmail.com>
2021-11-04 23:20:14 +0100

    Remove useless files

--- lego-house/lego.py
+++ lego-house/lego.py
@@ -1,10 +1,8 @@
 import os
-from random import randrange
 from time import sleep
 from picamera import PiCamera
-import base64
 
-from flask import Flask, send_file, send_from_directory, request
+from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_url_path='')
 
@@ -19,29 +17,11 @@ def get_image():
     if os.path.exists("download.jpg"):
         os.remove("download.jpg")
     with PiCamera() as camera:
-    #camera = PiCamera()
         camera.resolution = (1024, 768)
-    #camera.start_preview()
-    # Camera warm-up time
         sleep(2)
         camera.capture('download.jpg')
     
     return send_from_directory('./', "download.jpg")
-
-   #  filename = 'r2d2.jpg'
-    #index = randrange(4) + 1
-    #return send_from_directory('./', f"r2d2-{index}.jpg")
-    #return send_from_directory('./', f"r2d2-{index}.jpg", as_attachment=True)
-    #with open(f"r2d2-{index}.jpg", "rb") as image_file:
-    #    encoded_string = base64.b64encode(image_file.read())
-    #    # print(encoded_string)
-
-    # 1298: OK
-    # 1299: KO
-    #html = 'x' * 1299
-    #return f"<p>{html}</p>", 200
-    #return {"image": encoded_string.decode("utf8")}, 200
-    #  return send_file(filename, mimetype='image/gif')
 
 
 if __name__ == "__main__":
--- lego-house/run.sh
+++
@@ -1,1 +0,0 @@
-gunicorn -w 1 --bind 0.0.0.0:10001 --access-logfile /root/logs/logs --error-logfile /root/logs/logs wsgi:app | tail -f /root/logs/logs
