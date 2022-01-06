483587a97a61f698709859a94a46528d0062cdd9
mmyumu <mmyumu@gmail.com>
2022-01-06 22:29:43 +0100

    Fix playSounds slash commands in DM

--- bot/utils/discordUtils.js
+++ bot/utils/discordUtils.js
@@ -3,9 +3,18 @@ module.exports = {
 };
 
 function getMember(interaction, target) {
-    if (target) {
+    if (target && target.voice) {
         return target;
-    } else {
+    } else if (interaction.guild) {
         return interaction.guild.members.cache.get(interaction.member.user.id);
+    } else {
+        for (const guild of interaction.client.guilds.cache.map(g => g)) {
+            for (const member of guild.members.cache.map(m => m)) {
+                if (member.id === interaction.user.id) {
+                    return member;
+                }
+            }
+        }
     }
+    throw 'Member not found';
 }
